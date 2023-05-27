2638a2c17449883109003251dee95e5b1b16a8b3
NokuhleNgubane <111666514+Nokuhle@users.noreply.github.com>
2023-05-27 10:39:16 +0200

    table created for Admission Application

--- FormDetails.php
+++ FormDetails.php
@@ -9,21 +9,19 @@ $sql = "CREATE TABLE $tableName (
     First_Name VARCHAR(30) NOT NULL,
     Citizenship VARCHAR(30) NOT NULL,
     ID_Type VARCHAR(30) NOT NULL,
-    ID/Passport_Number INTEGER(30) NOT NULL,
-    Citizenship VARCHAR(30) NOT NULL,
+    ID_Passport_Number INTEGER(30) NOT NULL,
     ORCID_ID_Number INTEGER(30) NOT NULL,
     Race VARCHAR(30) NOT NULL,
     Gender VARCHAR(30) NOT NULL,
     Ref_No INTEGER(30) NOT NULL,
     DOB VARCHAR(30) NOT NULL,
-    Phd_Date DATE(30) NOT NULL,
+    Phd_Date DATE NOT NULL,
     Phd_Institution VARCHAR(30) NOT NULL,
     Home_Language VARCHAR(30) NOT NULL,
     NextOfKin_Name VARCHAR(30) NOT NULL,
     NextOfKin_contact INTEGER(30) NOT NULL,
     Disability VARCHAR(30) NOT NULL,
-    Disability_Description VARCHAR(50) NOT NULL,
-
+    Disability_Description VARCHAR(50) NOT NULL
 )";
 
 if ($conn->query($sql) === TRUE) {
@@ -33,12 +31,12 @@ if ($conn->query($sql) === TRUE) {
 }
 
 
-$tableName = "$UserContact";  
+$tableName = "UserContact";  
 $sql = "CREATE TABLE $tableName (
     UserID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     First_Name VARCHAR(30) NOT NULL,
     Initials VARCHAR(30) NOT NULL,
-    Postodctoral_Fellow VARCHAR(30) NOT NULL,
+    Postdoctoral_Fellow VARCHAR(30) NOT NULL,
     Department VARCHAR(30) NOT NULL,
     Faculty VARCHAR(30) NOT NULL,
     Physical_Address VARCHAR(30) NOT NULL,
@@ -51,8 +49,7 @@ $sql = "CREATE TABLE $tableName (
     Web_Address VARCHAR(30) NOT NULL,
     Country VARCHAR(30) NOT NULL,
     Province VARCHAR(30) NOT NULL,
-    Date_Signed DATE(30) NOT NULL,
-
+    Date_Signed DATE NOT NULL
 )";
 
 if ($conn->query($sql) === TRUE) {
@@ -60,6 +57,4 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "Error creating table: " . $conn->error;
 }
-
-
 ?>
